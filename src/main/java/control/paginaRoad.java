0b0c412bd38a23ee66dba937ef09c7dfffbd3130
castelll0009 <45212708+castelll0009@users.noreply.github.com>
2020-10-01 15:08:22 -0500

    Initial commit

---
+++ src/main/java/control/paginaRoad.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+/**
+ *
+ * @author Ivan Esteban Castill
+ */
+public class paginaRoad {
+    public String consultarClientes(){
+        //return  "{\"clave\":\"hola Mundo\"}"; //{clave:"hola mundo"}
+        
+        //conexion alservidor de base de datos
+        MongoClient  mongoClient = MongoClients.create("mongodb://127.0.0.0:27017");
+        //MongoClient  mongoClient = MongoClients.create("mongodb://servercastell:27017");
+        //conecxio a la base de datos
+        MongoDatabase database = mongoClient.getDatabase("Roadtosun");
+        // Instancia  de la collecion
+        MongoCollection<Document> collection = database.getCollection("empleados");
+       //comentario comit
+        //TODO:manejo adecuado si la coleccion no existe o esta vacia
+        return  collection.find().first().toJson();        
+
+    }
+
+}
+
+
---
+++ src/main/java/vista/WS_road.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista;
+
+import control.paginaRoad;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author Ivan Esteban Castill
+ */
+@Path("WS_road")
+public class WS_road {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of WS_road
+     */
+    public WS_road() {
+    }
+
+    @GET
+    @Path("consultarClientes")
+    @Produces({"application/json"})
+    public  String ConsultarClientes(){
+        paginaRoad miPagina = new paginaRoad();
+        return miPagina.consultarClientes();
+    }
+}
