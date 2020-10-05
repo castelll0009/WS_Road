30d02940271b4d04b228be61b2dcd9df0b28153f
castelll0009 <45212708+castelll0009@users.noreply.github.com>
2020-10-05 11:57:38 -0500

    Funciona el acceeso a la base de datos remota

--- src/main/java/control/paginaRoad.java
+++ src/main/java/control/paginaRoad.java
@@ -5,7 +5,8 @@
  */
 package control;
 
-import com.mongodb.client.MongoClient;
+//import com.mongodb.client.MongoClient;
+import com.mongodb.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
@@ -18,16 +19,26 @@ import org.bson.Document;
 public class paginaRoad {
     public String consultarClientes(){
         //return  "{\"clave\":\"hola Mundo\"}"; //{clave:"hola mundo"}
+        //conexon a una base de  datos  remota
+        MongoClient mongoClient;
+        MongoClientURI uri = new
+        MongoClientURI("mongodb://Lab3:passworduserLab3@93.188.167.110:27017/?authSource=lab3");
         
+        MongoDatabase db;
+        mongoClient = new MongoClient(uri);
+        db = mongoClient.getDatabase("lab3");
+        MongoCollection<Document> collection = db.getCollection("clientes");
+        //Mostrar primer registro
+        return collection.find().first().toJson();
         //conexion alservidor de base de datos
-        MongoClient  mongoClient = MongoClients.create("mongodb://127.0.0.0:27017");
+       // MongoClient  mongoClient = MongoClients.create("mongodb://127.0.0.0:27017");
         //MongoClient  mongoClient = MongoClients.create("mongodb://servercastell:27017");
         //conecxio a la base de datos
-        MongoDatabase database = mongoClient.getDatabase("Roadtosun");
+       // MongoDatabase database = mongoClient.getDatabase("Roadtosun");
         // Instancia  de la collecion
-        MongoCollection<Document> collection = database.getCollection("empleados");      
+        //MongoCollection<Document> collection = database.getCollection("empleados");      
         //TODO:manejo adecuado si la coleccion no existe o esta vacia
-        return  collection.find().first().toJson();        
+        //return  collection.find().first().toJson();                
 
     }
 
